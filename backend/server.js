485187491462bd00a1c778f1608c05449fec6e28
PauloMiguelVidal <paulo2.0miguel@gmail.com>
2026-09-02 08:43:04 -0300

    reajustando dominio

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: 'https://mern-project-ju129bjyu-paulomiguelvidals-projects.vercel.app/', // URL do seu frontend
+  origin: 'mern-project-iota-three.vercel.app', // URL do seu frontend
   credentials: true
 }));
 app.use(express.json());
